366b2d78a8001c7668c01adde20fd25ccf02c0f2
David Souza <david@ditcom.com.br>
2025-11-06 22:04:26 +0000

    setup tailwind / daisyui / icons

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <div></div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      Map <icon name="fa7-solid:bath" />
+    </button>
   </div>
 </template>
