35f006190fc78868f6700bb6237cd4161114deec
David Souza <david@ditcom.com.br>
2025-11-10 16:07:24 +0000

    Add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      Map <icon name="fa7-solid:bath" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
